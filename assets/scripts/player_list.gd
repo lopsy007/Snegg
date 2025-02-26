e10d58be9fd299b245a3ad6bc2c2073e4a9838d1
Eirik Moltubak <eirikmalme@icloud.com>
2025-02-26 09:26:59 +0100

    Fixed bug with DeletePlayerButton

--- assets/scripts/player_list.gd
+++ assets/scripts/player_list.gd
@@ -1,13 +1,13 @@
 extends VBoxContainer
 
 @onready var players = Global.players
-@onready var deleteButton = preload("res://assets/scenes/Leaderboard/DeletePlayerButton.tscn").instantiate()
+@onready var deleteButtonScene = preload("res://assets/scenes/Leaderboard/DeletePlayerButton.tscn")
+var deleteButton: Button
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	update_player_list()
-	get_children()[0].add_child(deleteButton)
-	
+	create_player_list()
+
 
 
 
@@ -19,13 +19,18 @@ func _process(delta: float) -> void:
 func _on_mouse_entered(player_label:RichTextLabel) -> void:
 	deleteButton.move_button_to(player_label)
 	
-func _on_mouse_exited() -> void:
-	print("exited (player_list.gd)")
+	
+func update_player_list() -> void:
+	delete_player_list()
+	# Wait for all player labels to be deleted before creating the next tree.
+	# REQUIRED for DeletePlayerButton to function correctly
+	# Will crash if removed
+	await get_children()[len(get_children())-1].tree_exited
+	create_player_list()
+	
+func create_player_list() -> void:
 	
 	
-func update_player_list():
-	for child in get_children():
-		child.queue_free()
 	var player_label: RichTextLabel
 	for pl in players:
 		player_label = RichTextLabel.new()
@@ -34,3 +39,11 @@ func update_player_list():
 		player_label.scroll_active = false
 		add_child(player_label)
 		player_label.mouse_entered.connect(_on_mouse_entered.bind(player_label))
+	
+	deleteButton = deleteButtonScene.instantiate()
+	get_children()[0].add_child(deleteButton)
+	
+	
+func delete_player_list() -> void:
+	for child in get_children():
+		child.queue_free()
