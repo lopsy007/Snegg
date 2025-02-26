bb749b3ccfb8987596ea07a2123ea7ce4b41604b
Eirik Moltubak <eirikmalme@icloud.com>
2025-02-26 09:21:50 +0100

    Removed print debugs and added condition for adding player

--- assets/scripts/player_add_bar.gd
+++ assets/scripts/player_add_bar.gd
@@ -5,11 +5,9 @@ signal player_list_updated
 
 
 func _on_confirm_button_pressed() -> void:
-	if text.strip_edges() != "": 
+	if text.strip_edges() != "" and text.strip_edges() not in Global.players.keys(): 
 		Player_data.new(text.strip_edges())
 	player_list_updated.emit()
-	print("players")
-	print(Global.players)
 	text = ""
 
 
