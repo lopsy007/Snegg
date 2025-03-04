41d556998232acdb2034a8494981835207f3e324
Eirik Moltubak <eirikmalme@icloud.com>
2025-03-04 10:24:22 +0100

    Bug fix, reaction when illegal add

--- assets/scripts/player_add_bar.gd
+++ assets/scripts/player_add_bar.gd
@@ -7,8 +7,8 @@ signal player_list_updated
 func _on_confirm_button_pressed() -> void:
 	if text.strip_edges() != "" and text.strip_edges() not in Global.players.keys(): 
 		Player_data.new(text.strip_edges())
-	player_list_updated.emit()
-	text = ""
+		player_list_updated.emit()
+		text = ""
 
 
 
