b9ee752d591ac98dbd7614d0b2a19c47b00f622a
Eirik Moltubak <eirikmalme@icloud.com>
2025-03-04 10:31:34 +0100

    🚀 Fixed button auto focus!

--- assets/scripts/player_list.gd
+++ assets/scripts/player_list.gd
@@ -1,35 +1,30 @@
+class_name DeletePlayerDataButton
 extends VBoxContainer
 
 @onready var players = Global.players
 @onready var deleteButtonScene = preload("res://assets/scenes/Leaderboard/DeletePlayerButton.tscn")
+var label_children: Array
 var deleteButton: TextureButton
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	create_player_list()
+	update_player_list()
 
 
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
 
-
-func _on_mouse_entered(player_label:RichTextLabel) -> void:
-	deleteButton.move_button_to(player_label)
+func _on_mouse_entered(deleteButton:TextureButton) -> void:
+	deleteButton.disabled = false
 	
 	
+func _on_mouse_exited(deleteButton:TextureButton) -> void:
+	deleteButton.disabled = true
+
+
 func update_player_list() -> void:
-	delete_player_list()
-	# Wait for all player labels to be deleted before creating the next tree.
-	# REQUIRED for DeletePlayerButton to function correctly
-	# Will crash if removed
-	await get_children()[len(get_children())-1].tree_exited
-	create_player_list()
-	
-func create_player_list() -> void:
-	
+	for child in get_children():
+		child.queue_free()
 	
 	var player_label: RichTextLabel
 	for pl in players:
@@ -37,14 +32,16 @@ func create_player_list() -> void:
 		player_label.text = pl
 		player_label.custom_minimum_size = Vector2(0, 20)
 		player_label.scroll_active = false
+		
+		
+		deleteButton = deleteButtonScene.instantiate()
+		player_label.mouse_entered.connect(_on_mouse_entered.bind(deleteButton))
+		player_label.mouse_exited.connect(_on_mouse_exited.bind(deleteButton))
+		deleteButton.mouse_entered.connect(_on_mouse_entered.bind(deleteButton))
+		deleteButton.mouse_exited.connect(_on_mouse_exited.bind(deleteButton))
+		deleteButton.disabled = true
+		
+		player_label.add_child(deleteButton)
 		add_child(player_label)
-		player_label.mouse_entered.connect(_on_mouse_entered.bind(player_label))
+
 	
-	deleteButton = deleteButtonScene.instantiate()
-	get_children()[0].add_child(deleteButton)
-	
-	
-	
-func delete_player_list() -> void:
-	for child in get_children():
-		child.queue_free()
